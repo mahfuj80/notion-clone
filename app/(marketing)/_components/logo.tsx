d29a68e5fc91bcfa842b36f23e987430c991c85b
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-10-14 14:20:04 +0600

    Markegin page created and designed responsively for notion

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({ subsets: ["latin"], weight: ["400", "600"] });
+
+export const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image height={40} width={40} src={"/logo.svg"} alt="Logo" />
+      <p className={cn("font-semibold", font.className)}>Notion</p>
+    </div>
+  );
+};
